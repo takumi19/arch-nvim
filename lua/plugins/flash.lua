316e05379ade86bf876317fa60220741e750a82b
takumi19 <terrafin03@gmail.com>
2024-12-08 00:00:09 +0300

    add flash and todo-comments, change neotree,

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,21 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {
+    modes = {
+      char = {
+        enabled = true,
+        highlight = {
+          backdrop = false,
+          matches = false,
+          -- comment out the following line to enable character highlighting
+          groups = { "" },
+        },
+      }
+    }
+  },
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+  }
+}
---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,26 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    -- signs = false,
+    keywords = {
+      LAB = { color = "info", alt = { "LAB" } },
+    },
+    highlight = {
+      pattern = [[.*<(KEYWORDS)]], -- pattern or table of patterns, used for highlighting (vim regex)
+    },
+    search = {
+      command = "rg",
+      args = {
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+      },
+      -- regex that will be used to match keywords.
+      -- don't replace the (KEYWORDS) placeholder
+      pattern = [[\b(KEYWORDS(\s\w)?):]], -- ripgrep regex
+    }
+  },
+}
